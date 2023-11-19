cb86ac545802b7d8a8f990c5a8bae211851c24ed
denjayx <denyw602@gmail.com>
2023-11-19 15:25:10 +0700

    init: flowbite

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
