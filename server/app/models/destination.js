8bad5a700f099b211fd412584b6343ddaee2d9e3
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-20 22:31:04 +0700

    init database: create model and migration

---
+++ server/app/models/destination.js
@@ -0,0 +1,53 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Destination extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.PackageDetails = Destination.hasMany(models.PackageDetail, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        foreignKey: {
+          name: 'destinationId',
+          allowNull: false,
+        },
+      });
+    }
+  }
+  Destination.init(
+    {
+      destinationId: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+      },
+      destinationName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      imageUrl: {
+        type: DataTypes.STRING,
+      },
+      city: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      description: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Destination',
+      tableName: 'destinations',
+      underscored: true,
+      paranoid: true,
+    },
+  );
+  return Destination;
+};
