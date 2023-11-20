1b3efc27cc1bc7c0e509d30e42884e7a18dbc2ff
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-20 22:31:04 +0700

    init database: create model and migration

---
+++ server/app/models/user.js
@@ -0,0 +1,90 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.Account = User.belongsTo(models.Account, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        foreignKey: {
+          name: 'accountId',
+          allowNull: false,
+        },
+      });
+      this.Packages = User.hasMany(models.Package, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        foreignKey: {
+          name: 'tourGuideId',
+          allowNull: false,
+        },
+      });
+      this.Transactions = User.hasMany(models.Transaction, {
+        onUpdate: 'cascade',
+        onDelete: 'cascade',
+        foreignKey: {
+          name: 'touristId',
+          allowNull: false,
+        },
+      });
+    }
+  }
+  User.init(
+    {
+      username: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.STRING,
+      },
+      accountId: {
+        allowNull: false,
+        unique: true,
+        type: DataTypes.UUID,
+      },
+      avatarUrl: {
+        type: DataTypes.STRING,
+      },
+      firstName: {
+        type: DataTypes.STRING,
+      },
+      lastName: {
+        type: DataTypes.STRING,
+      },
+      biography: {
+        type: DataTypes.TEXT,
+      },
+      nik: {
+        type: DataTypes.STRING,
+      },
+      phone: {
+        type: DataTypes.STRING,
+      },
+      province: {
+        type: DataTypes.STRING,
+      },
+      city: {
+        type: DataTypes.STRING,
+      },
+      gender: {
+        type: DataTypes.ENUM,
+        values: ['L', 'P'],
+      },
+      cardNumber: {
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users',
+      underscored: true,
+      paranoid: true,
+    },
+  );
+  return User;
+};
---
+++ server/migrations/20231120094937-create-account.js
@@ -0,0 +1,50 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('accounts', {
+      accountId: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        field: 'account_id',
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+        field: 'email',
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        field: 'password',
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.ENUM,
+        values: ['tourGuide', 'tourist'],
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: 'updated_at',
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        field: 'deleted_at',
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('accounts');
+  },
+};
