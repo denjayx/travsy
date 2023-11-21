05a84b5e7362b3573c6c35bb27c279b858d187ad
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-22 06:00:32 +0700

    route init: create get package list route

--- server/app/controllers/index.js
+++ server/app/controllers/index.js
@@ -1,6 +1,26 @@
-const {
-  exampleController,
-  exampleControllerError,
-} = require('./exampleController');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = { exampleController, exampleControllerError };
+try {
+  fs.readdirSync(__dirname)
+    .filter(
+      (file) =>
+        file.indexOf('.') !== 0 &&
+        file !== path.basename(__filename) &&
+        file.slice(-3) === '.js' &&
+        file.indexOf('.test.js') === -1,
+    )
+    .forEach((file) => {
+      try {
+        const controllers = require(path.join(__dirname, file));
+
+        Object.keys(controllers).forEach((key) => {
+          module.exports[key] = controllers[key];
+        });
+      } catch (error) {
+        console.error(`Error importing module from ${file}:`, error);
+      }
+    });
+} catch (error) {
+  console.error('Error reading directory:', error);
+}
--- server/app/services/PackageService.js
+++ server/app/services/PackageService.js
@@ -1,9 +1,17 @@
-const DatabaseService = require('./DatabaseService');
 const { Op } = require('sequelize');
-const { Package, Destination, City } = require('../models');
+const DatabaseService = require('./DatabaseService');
+const { Package } = require('../models');
 
 class PackageService extends DatabaseService {
-  static async getPackageList(filter) {
+  static getInstance() {
+    if (!PackageService.instance) {
+      PackageService.instance = new PackageService();
+    }
+
+    return PackageService.instance;
+  }
+
+  async getPackageList(filter) {
     const expectedFilter = [
       'search',
       'city',
@@ -21,22 +29,43 @@ class PackageService extends DatabaseService {
       }
     });
 
-    const {
-      search = '',
-      city = '',
-      pmin = '',
-      pmax = '',
-      ndest = '',
-      sdate = '',
-      edate = '',
-      sort = '',
-    } = filter;
+    const { search = '', pmin = 0, pmax = 0 } = filter;
 
-    const getFilteredPackages = async (transaction) => {
-      const packages = await Package.findAll({}, { transaction });
+    const whereClauses = [];
+
+    whereClauses.push({
+      packageName: {
+        [Op.like]: `%${search}%`,
+      },
+    });
+
+    if (pmax !== 0) {
+      whereClauses.push({
+        price: {
+          [Op.between]: [pmin, pmax],
+        },
+      });
+    }
+
+    const findPackagesWithFilters = async (transaction) => {
+      const packages = await Package.findAll(
+        {
+          where: {
+            [Op.and]: whereClauses,
+          },
+        },
+        { transaction },
+      );
 
       return packages;
     };
+
+    const result = await this.doTransaction(
+      findPackagesWithFilters,
+      'READ_COMMITTED',
+    );
+
+    return result;
   }
 }
 
