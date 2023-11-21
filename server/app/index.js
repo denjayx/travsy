253363b06d59bb98903b8ca45201d92aa5cca938
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-21 20:22:05 +0700

    refactor code: separate app and entry point

--- server/app/index.js
+++ server/app/index.js
@@ -1,11 +1,5 @@
-const express = require('express');
 const { host, port } = require('../config/application');
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+const app = require('./app');
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
