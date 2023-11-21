4e11d8e65c6d86f35830f14349b1e27ed5926499
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-21 20:22:05 +0700

    refactor code: separate app and entry point

---
+++ server/app/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to Travsy' });
+});
+
+module.exports = app;
