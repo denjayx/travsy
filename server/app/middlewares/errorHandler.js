fbb88671b1cf0fea304da2c3778db58d03cb8d13
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-11-22 21:26:44 +0700

    fix bug: add next parameter

--- server/app/middlewares/errorHandler.js
+++ server/app/middlewares/errorHandler.js
@@ -1,4 +1,5 @@
-const errorHandler = (err, req, res) => {
+// eslint-disable-next-line no-unused-vars
+const errorHandler = (err, req, res, next) => {
   console.log(err);
   res.status(err.statusCode).json({
     status: 'error',
