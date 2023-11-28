9d32a6b6f299b30396e02c399e54d0492fde6a25
denjayx <denyw602@gmail.com>
2023-11-29 00:54:13 +0700

    init: alpine js

--- client/index.js
+++ client/index.js
@@ -2,3 +2,8 @@ import 'flowbite';
 import 'flowbite/dist/datepicker';
 import './src/styles/app.css';
 import './src/scripts/routes/Router';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
