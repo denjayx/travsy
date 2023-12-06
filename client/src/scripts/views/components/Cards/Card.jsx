753b4e4e7761933affd996460227228179be8cd7
denjayx <denyw602@gmail.com>
2023-12-06 23:53:17 +0700

    feat: route to detail page

--- client/src/scripts/views/components/Cards/Card.jsx
+++ client/src/scripts/views/components/Cards/Card.jsx
@@ -2,8 +2,9 @@
 import CardImage from '../../../../assets/images/card-image.png'
 import Avatar from '../../../../assets/avatar.png'
 import Labels from '../Labels/Lables'
+import { Link } from 'react-router-dom'
 
-const Card = ({ packagesData, tourguideData, labelData }) => {
+const Card = ({ packagesData, tourguideData, labelData, cardId }) => {
   const formatCurrency = (amount) => {
     return new Intl.NumberFormat('id-ID', {
       style: 'currency',
@@ -14,33 +15,35 @@ const Card = ({ packagesData, tourguideData, labelData }) => {
 
   return (
     <article className="flex flex-col gap-4 rounded-2xl bg-white p-6 max-xl:flex-shrink-0 max-md:w-full md:w-6/12 lg:w-4/12">
-      <figure className="h-64 w-full overflow-hidden rounded-lg">
-        <img
-          src={CardImage}
-          alt="Thumbnail Card"
-          className="h-full w-full object-cover"
-        />
-      </figure>
-      <header className="tourguide flex items-center justify-start gap-2">
-        <img
-          src={Avatar}
-          width="32px"
-          alt="tourguide avatar"
-          className="avatar"
-        />
-        <span className="text-sm text-primary-950">
-          {`${tourguideData.firstName} ${tourguideData.lastName}`}
+      <Link to={`/packages/${cardId}`}>
+        <figure className="h-64 w-full overflow-hidden rounded-lg">
+          <img
+            src={CardImage}
+            alt="Thumbnail Card"
+            className="h-full w-full object-cover"
+          />
+        </figure>
+        <header className="tourguide flex items-center justify-start gap-2">
+          <img
+            src={Avatar}
+            width="32px"
+            alt="tourguide avatar"
+            className="avatar"
+          />
+          <span className="text-sm text-primary-950">
+            {`${tourguideData.firstName} ${tourguideData.lastName}`}
+          </span>
+        </header>
+        <h4 className="text-md font-semibold text-primary-950">
+          {`${packagesData.packageName.substr(0, 120)}...`}
+        </h4>
+        <span className="text-md font-semibold text-primary-600">
+          {`${formatCurrency(packagesData.price)}`}
         </span>
-      </header>
-      <h4 className="text-md font-semibold text-primary-950">
-        {`${packagesData.packageName.substr(0, 120)}...`}
-      </h4>
-      <span className="text-md font-semibold text-primary-600">
-        {`${formatCurrency(packagesData.price)}`}
-      </span>
-      <footer>
-        <Labels {...labelData} />
-      </footer>
+        <footer>
+          <Labels {...labelData} />
+        </footer>
+      </Link>
     </article>
   )
 }
---
+++ client/src/scripts/views/pages/Detail.jsx
@@ -0,0 +1,9 @@
+const Detail = () => {
+  return (
+    <div>
+      <h2>Detail Page</h2>
+    </div>
+  )
+}
+
+export default Detail
