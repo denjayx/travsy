86971666056e4f1e0924dae1dbfaf3d55de3f042
Pijar Pahlawan <pijarpahlawan@students.amikom.ac.id>
2023-12-09 20:07:05 +0700

    fix: bug in file uploading

--- server/src/middlewares/uploadImage.js
+++ server/src/middlewares/uploadImage.js
@@ -1,8 +1,13 @@
 const multer = require('multer');
+const fs = require('fs');
 const { host, port } = require('../../config/application');
 const ServerError = require('../errors/ServerError');
 
 const uploadImage = (destination, fileKey) => {
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination, { recursive: true });
+  }
+
   const storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, destination);
