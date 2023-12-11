e68936822fa638ef036d1f1ad92000070c318975
pijarpahlawan <95151798+pijarpahlawan@users.noreply.github.com>
2023-12-11 20:41:42 +0700

    refactor: change function name

---
+++ server/src/controllers/getHistoryDetailController.js
@@ -0,0 +1,25 @@
+const TransactionService = require('../services/TransactionService');
+
+const getHistoryDetailController = async (req, res, next) => {
+  const { username } = req.user;
+  const { id: transactionId } = req.params;
+
+  try {
+    const transactionService = TransactionService.getInstance();
+    const historyDetail =
+      await transactionService.getDetailHistoryTransactionByUsername(
+        username,
+        transactionId,
+      );
+
+    res.status(200).json({
+      status: 'OK',
+      message: 'Successfully got the detail transaction',
+      data: historyDetail,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getHistoryDetailController;
