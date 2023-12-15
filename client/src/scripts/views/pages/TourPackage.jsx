2a812556d103286295d46bec221dc8538356ecbf
denjayx <denyw602@gmail.com>
2023-12-15 08:35:39 +0700

    feat: layouting popular packages and filter box wisata page

--- client/src/scripts/views/pages/TourPackage.jsx
+++ client/src/scripts/views/pages/TourPackage.jsx
@@ -1,12 +1,14 @@
 import BookingFields from '../components/BookingFields/BookingFields'
+import SideBarFilter from '../components/BookingFields/SideBarFilter'
 import PopularPackage from '../components/Popular/PopularPackage'
 
 export default function TourPackage() {
   return (
     <section className="mt-32">
       <BookingFields />
-      <div className="flex">
-        <PopularPackage className="mt-16" />
+      <div className="container mt-16 flex gap-4">
+        <PopularPackage />
+        <SideBarFilter />
       </div>
     </section>
   )
