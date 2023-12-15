ba6dd457b408dc31e4743e3fc094e8ce1f49c92e
Yoga Tandean <yogagawoh01@gmail.com>
2023-12-15 19:46:50 +0700

    feat: Add list history package

--- client/src/scripts/routes/router.jsx
+++ client/src/scripts/routes/router.jsx
@@ -6,6 +6,7 @@ import {
 import RootLayout from '../views/layouts/RootLayout'
 import TourPackage from '../views/pages/TourPackage'
 import PackageHistory from '../views/pages/PackageHistory'
+import HistoryDetails from '../views/pages/HistoryDetails'
 import Login from '../views/pages/Login'
 import Register from '../views/pages/Register'
 import Homepage from '../views/pages/Homepage'
@@ -22,6 +23,7 @@ const router = createBrowserRouter(
           <Route index element={<Homepage />}></Route>
           <Route path="/package" element={<TourPackage />}></Route>
           <Route path="/history" element={<PackageHistory />}></Route>
+          <Route path="/history/detail" element={<HistoryDetails />}></Route>
           <Route path="/packages/:id" element={<Detail />}></Route>
           <Route path="/order" element={<OrderDetails />}></Route>
         </Route>
