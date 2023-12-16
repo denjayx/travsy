634e5cec901bbf1322624e02eed37180e5629b2b
RahmanIslamIen <rahmanislamien@gmail.com>
2023-12-16 19:29:59 +0700

    feat : upload gambar profile, hapus sidebar

--- client/src/scripts/views/layouts/Dashboard.jsx
+++ client/src/scripts/views/layouts/Dashboard.jsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import Sidebar from '../pages/Dashboard/Sidebar'
 
 const Dashboard = () => {
   return (
     <>
       <div className="mt-32 p-4 sm:ml-64">
-        <Sidebar />
         <Outlet />
       </div>
     </>
--- client/src/scripts/views/pages/Dashboard/UpdateProfile.jsx
+++ client/src/scripts/views/pages/Dashboard/UpdateProfile.jsx
@@ -7,6 +7,42 @@ const UpdateProfile = () => {
         <form>
           <div className="mb-6 text-xl font-extrabold">Profile Anda</div>
           <div className="mb-4">
+            <center>
+              <label className="hover:bg-blue flex w-64 cursor-pointer flex-col items-center rounded-lg border px-4 py-6 uppercase tracking-wide shadow-lg">
+                <svg
+                  className="h-6 w-6 text-white dark:text-gray-800"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 18"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M18 1H2a1 1 0 0 0-1 1v14a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                  />
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 5.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0ZM7.565 7.423 4.5 14h11.518l-2.516-3.71L11 13 7.565 7.423Z"
+                  />
+                </svg>
+                <span className="mt-2 text-base leading-normal">
+                  Plih Gambar Profile
+                </span>
+                <input type="file" className="hidden" id="image" />
+              </label>
+            </center>
+          </div>
+          <div className="mb-4">
             <label
               className="mb-2 block text-sm font-bold text-gray-700"
               htmlFor="username"
