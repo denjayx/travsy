a254a2da0d4b41cecd6da07c795e4c1853f83ece
denjayx <denyw602@gmail.com>
2023-12-16 18:11:36 +0700

    feat: error validation register

--- client/src/scripts/data/api.js
+++ client/src/scripts/data/api.js
@@ -134,8 +134,7 @@ export const register = async ({ user, account }) => {
 
     return response.data.data
   } catch (error) {
-    console.error('Error fetching register:', error)
-    throw error
+    throw new Error(error.response.data.message)
   }
 }
 
