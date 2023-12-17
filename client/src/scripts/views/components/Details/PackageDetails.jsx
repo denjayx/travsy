9a2ed279e4f18a6019bda98b68a5489741be63eb
pijarpahlawan <95151798+pijarpahlawan@users.noreply.github.com>
2023-12-18 01:24:16 +0700

    fix: showing image

--- client/src/scripts/views/components/Details/PackageDetails.jsx
+++ client/src/scripts/views/components/Details/PackageDetails.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
-import CardImage from '../../../../assets/images/card-image.png'
-import Avatar from '../../../../assets/avatar.png'
 import OrderForm from './OrderForm'
 import { useState } from 'react'
+import { BASE_IMAGEURL } from '../../../data/api'
+import defaultUser from '../../../../assets/default-user.svg'
 
 const PackageDetails = ({ packageData }) => {
   const [showOrderForm, setShowOrderForm] = useState(false)
@@ -26,7 +26,7 @@ const PackageDetails = ({ packageData }) => {
     >
       <div className="h-full w-full overflow-hidden rounded-lg md:rounded-l-lg lg:w-5/12">
         <img
-          src={packageData.package.thumbnailUrl}
+          src={BASE_IMAGEURL + packageData.package.thumbnailUrl}
           alt="Thumbnail Card"
           className=" h-full w-full object-cover "
         />
@@ -34,7 +34,11 @@ const PackageDetails = ({ packageData }) => {
       <section className="flex h-full w-full flex-col justify-between gap-1 rounded-lg border-gray-200 bg-white p-5 md:rounded-l-lg">
         <div className="flex items-center">
           <img
-            src={packageData.tourGuide.avatarUrl}
+            src={
+              packageData.tourGuide.avatarUrl
+                ? BASE_IMAGEURL + packageData.tourGuide.avatarUrl
+                : defaultUser
+            }
             width="32px"
             alt="tourguide avatar"
             className="avatar"
