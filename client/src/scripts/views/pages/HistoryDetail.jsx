159dbd4fef2c99dc16424715858f49ca46620038
denjayx <denyw602@gmail.com>
2023-12-18 15:25:43 +0700

    feat: history detail

--- client/src/scripts/views/pages/HistoryDetail.jsx
+++ client/src/scripts/views/pages/HistoryDetail.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import CardImageFull from '../../../assets/images/card-image-full.png'
-import Avatar from '../../../assets/avatar.png'
 import { FaCalendarDays } from 'react-icons/fa6'
 import { useParams } from 'react-router-dom'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { getHistoryDetail } from '../../data/api'
+import { BASE_IMAGEURL, getHistoryDetail } from '../../data/api'
 import { useOutletContext } from 'react-router-dom'
+import { formatCurrency } from '../../../utils/utils'
+import { Link } from 'react-router-dom'
 
 const HistoryDetail = () => {
   const { id } = useParams()
@@ -42,62 +42,38 @@ const HistoryDetail = () => {
     }
   }, [user, id])
 
-  useEffect(() => {
-    console.log(historyDetail)
-  }, [historyDetail])
+  useEffect(() => {}, [historyDetail])
 
   return (
-    <div className="item container mt-32 flex flex-col gap-2 lg:flex-row ">
-      <section
-        id="packagehistory"
-        className=" container mx-3 flex w-full flex-col items-center gap-7 rounded-xl bg-white p-6 "
-      >
-        <div className="w-full overflow-hidden rounded-lg md:rounded-l-lg ">
-          <img
-            src={CardImageFull}
-            alt="Thumbnail Card"
-            className=" h-full w-full object-cover "
-          />
-        </div>
-        <section className="flex w-full flex-col gap-1 rounded-lg border-gray-200  ">
-          <div className="flex items-center">
-            <img
-              src={Avatar}
-              width="32px"
-              alt="tourguide avatar"
-              className="avatar"
-            />
-            <span>{historyDetail.tourGuide.firstName}</span>
+    <div className="item container mt-32 flex flex-col gap-2 text-primary-950 lg:flex-row">
+      <div className="mx-3 flex h-full w-full flex-col gap-1 rounded-xl bg-white p-6 ">
+        <div className="flex w-full flex-col gap-3 rounded-lg border-gray-200">
+          <div className="flex items-center gap-2">
+            <div className="flex w-8 items-center overflow-hidden rounded-full">
+              <img
+                src={BASE_IMAGEURL + historyDetail.tourGuide.avatarUrl}
+                alt="tourguide avatar"
+                className="avatar"
+              />
+            </div>
+            <span>{`${historyDetail.tourGuide.firstName} ${historyDetail.tourGuide.lastName}`}</span>
           </div>
           <h5 className="mb-2 text-xl font-bold tracking-tight text-primary-950">
-            Pura Tanah Lot - Pura Uluwatu - Pantai Kuta
+            {historyDetail.transaction.package.packageName}
           </h5>
-          <p className="mb-3 font-normal text-gray-700 ">
-            Lorem ipsum dolor sit amet consectetur. Convallis montes nulla cras
-            in. Ut viverra odio et pharetra dictum nunc ultrices. Mattis lorem
-            amet egestas blandit ut. Sed hendrerit convallis scelerisque arcu
-            pulvinar id at.
-          </p>
-          <p className="mb-3 font-normal text-primary-950">
-            <span className="font-bold">3 Hari</span> Layanan
-          </p>
-          <section className="mb-4 flex flex-col items-center justify-between gap-2  md:flex-row">
-            <h4 className="text-xl font-bold text-primary-500">Rp500.000,-</h4>
+          <section className="flex flex-col items-center justify-between gap-2  md:flex-row">
+            <h4 className="text-xl font-bold text-primary-500">
+              {`${formatCurrency(historyDetail.transaction.totalPrice)}`}
+            </h4>
           </section>
-        </section>
-      </section>
-
-      <div className=" mx-3 flex h-full w-2/4 flex-col gap-1 rounded-xl bg-white p-6 ">
-        <h4 className="mb-2 text-md font-semibold text-primary-950">
-          Data Pesanan
-        </h4>
+        </div>
         <div className="mb-1 flex flex-col items-center gap-1">
           <div className="relative flex w-full flex-col">
             <label className="mb-2 text-base font-normal text-gray-600">
               Tanggal Mulai
             </label>
-            <p className="w-full rounded-xl border border-primary-200  px-4 py-2 text-primary-950">
-              2023-12-08
+            <p className="w-full rounded-xl border border-gray-200  px-4 py-2 text-primary-950">
+              {historyDetail.transaction.startDate}
             </p>
             <div className="pointer-events-none absolute inset-y-0 right-0 top-8 flex items-center px-3">
               <FaCalendarDays className="h-5 w-5 text-gray-500 " />
@@ -108,8 +84,8 @@ const HistoryDetail = () => {
             <label className="mb-2 text-base font-normal text-gray-600">
               Tanggal Selesai
             </label>
-            <p className="w-full rounded-xl border border-primary-200  px-4 py-2 text-primary-950">
-              2023-12-08
+            <p className="w-full rounded-xl border border-gray-200  px-4 py-2 text-primary-950">
+              {historyDetail.transaction.endDate}
             </p>
             <div className="pointer-events-none absolute inset-y-0 right-0 top-8 flex items-center px-3">
               <FaCalendarDays className="h-5 w-5 text-gray-500 " />
@@ -122,8 +98,8 @@ const HistoryDetail = () => {
           </label>
           <div className=" flex items-center ">
             <div className="relative flex w-full">
-              <p className="relative w-full rounded-xl border border-primary-200 px-4 py-2 text-primary-950">
-                1
+              <p className="relative w-full rounded-xl border border-gray-200 px-4 py-2 text-primary-950">
+                {historyDetail.transaction.totalPerson}
               </p>
             </div>
           </div>
@@ -134,21 +110,20 @@ const HistoryDetail = () => {
           </label>
           <div className=" flex items-center ">
             <div className="relative flex w-full">
-              <p className="relative w-full rounded-xl border border-primary-200 px-4 py-2 text-primary-950">
+              <p className="relative w-full rounded-xl border border-gray-200 px-4 py-2 text-primary-950">
                 2023-12-08
               </p>
             </div>
           </div>
         </div>
-        <div className="text-center">
+        <Link to={'/packages'} className="text-center">
           <button
             type="submit"
             className="w-full rounded-3xl bg-primary-500 px-6 py-3 text-center text-base font-normal text-white hover:bg-primary-600 focus:outline-none focus:ring-4 focus:ring-blue-300 "
-            // onClick={handleConfirm}
           >
             Pesan Lagi
           </button>
-        </div>
+        </Link>
       </div>
     </div>
   )
